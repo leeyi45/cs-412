cf9e0cfb2b0f62ed16a26de699312582eb90fb99
Lee Yi <leeyi45@gmail.com>
2025-09-09 17:44:12 -0400

    Finish doing assignment 1?

--- quotes/views.py
+++ quotes/views.py
@@ -6,10 +6,14 @@ from django.http import HttpRequest
 from django.shortcuts import render
 
 QUOTES = [
-  "I never think about the future"
+  "The best argument against democracy is a five-minute conversation with the average voter.",
+  "Tact is the ability to tell someone to go to hell in such a way that they look forward to the trip.",
+  "I am fond of pigs. Dogs look up to us. Cats look down on us. Pigs treat us as equals."
 ]
 IMAGES = [
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Albert_Einstein_1947.jpg/1920px-Albert_Einstein_1947.jpg"
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Sir_Winston_Churchill_-_19086236948.jpg/500px-Sir_Winston_Churchill_-_19086236948.jpg",
+  "https://cdn.britannica.com/25/171125-050-94459F61/Winston-Churchill.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKiWYBgPBMLR_FbmvJnoGvAJsAiWhoFMzjbiT21RHl6bMYVEPjAG6YeJU&s"
 ]
 
 def about(req: HttpRequest):
